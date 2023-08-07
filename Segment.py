2b738788b8ec8b0049d057c7e15663fa440d95b6
Simeon Ifalore <ifaloresimeon@gmail.com>
2023-08-07 10:15:47 +0100

    Update Segment.py

--- Segment.py
+++ Segment.py
@@ -27,7 +27,7 @@ segment_mapping = {
 
 # Create the Streamlit app
 def main():
-    st.title(":green[Customer Segmentation using RFM Analysis]")
+    st.title(":purple[Customer Segmentation using RFM Analysis]")
     st.image(image, width=500)
 
     # Input feature values from the user
